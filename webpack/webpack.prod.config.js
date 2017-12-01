4d7ed96a1445399fdec7f76f3fbdd56656b34da8
francis jiang <jhjguxin@gmail.com>
2017-12-01 14:36:31 +0800

    config newrelic

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -103,7 +103,8 @@ module.exports = (env = {}) => {
       }),
       new CopyWebpackPlugin(
         [
-          { from: './assets/images', to: './images/' }
+          { from: './assets/images', to: './images/' },
+          { from: "./vendor/javascripts", to: "./vendor/" }
         ],
         { copyUnmodified: false }
       )
