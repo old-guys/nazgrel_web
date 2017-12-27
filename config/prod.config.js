7e9579d741bbdf14576fff475a534b64891547c8
lra <1261847034@qq.com>
2017-12-27 11:02:31 +0800

    config/prod.config.js restore

--- config/prod.config.js
+++ config/prod.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   buildPath: '/',
   publicPath: '/',
-  apiDomain: 'http://qa.bi.ishanggang.com/',
+  apiDomain: 'http://bi.ishanggang.com/',
   accessToken: '9ad4a66396ac9fdd8161f77a4b9e0b7cc38c192e4ceccfcbc40fb2fccc85e544066f743e33726c51b2223b',
   userToken: 'Xts4/A5ax5rPAsP1aFj7Z9Y/cwgvJGSFkVoET10oWDQ=',
   debug: true
