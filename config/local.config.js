b8e29c5bea8a41479235bbefc0d7be0f3ca1e4da
francis jiang <jhjguxin@gmail.com>
2018-02-11 11:06:54 +0800

    Collect prod, local, test exception log into Sentry

--- config/local.config.js
+++ config/local.config.js
@@ -5,7 +5,7 @@ module.exports = {
   accessToken: '9ad4a66396ac9fdd8161f77a4b9e0b7cc38c192e4ceccfcbc40fb2fccc85e544066f743e33726c51b2223b',
   userToken: '97e261ee91276fb352b0d73e84f4e851',
   debug: true,
-  sentryApmUrl: 'http://7f7a56d2a6294e1faab594340ef1f466@localhost:8080/2',
+  sentryApmUrl: 'http://307fd5ef8d414ad689a7514b47971282@106.15.91.158:8080/2',
 
   fayeHost: 'http://localhost:3007/'
 };
