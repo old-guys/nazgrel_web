5b433938feefa064584c5f88c7e3481294c6f71e
francis jiang <jhjguxin@gmail.com>
2018-03-08 11:18:52 +0800

    Import component use alias path

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
-import Header from '../../components/Header/';
-import Sidebar from '../../components/Sidebar/';
-import Breadcrumb from '../../components/Breadcrumb/';
-import Aside from '../../components/Aside/';
-import Footer from '../../components/Footer/';
+import Header from 'components/Header/';
+import Sidebar from 'components/Sidebar/';
+import Breadcrumb from 'components/Breadcrumb/';
+import Aside from 'components/Aside/';
+import Footer from 'components/Footer/';
 
 import Dashboard from '../../views/Dashboard/';
 
